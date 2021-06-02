eb557f6e2d31aa553b9eafa6ebf5dc6c6bdd5768
Zihad <zihadnayem8@gmail.com>
2021-06-02 12:40:13 +0600

    intialize project setup and install bootstrap4, fontawesome.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="container">
+      <h2>Hello world</h2>
+    </main>
+  );
+}
+
+export default App;
